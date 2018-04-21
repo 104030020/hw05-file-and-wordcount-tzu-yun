1bf9a88b5154294a5593c14580b6bc3b7dc6a96f
104030020 <37045708+104030020@users.noreply.github.com>
2018-04-21 21:07:39 +0800

    Add files via upload
    fisrt upload

--- hw5.py
+++ hw5.py
@@ -1,9 +1,17 @@
-# -*- coding: utf-8 -*-
+
+# coding: utf-8
+
+# In[1]:
+
+
 import csv
 import json
 import pickle
 from collections import Counter
 import string
+
+
+# In[2]:
 
 
 def main(filename):
@@ -14,43 +22,39 @@ def main(filename):
     for line in lines:
         words = line.split()
 
-        # check the format of words and append it to "all_words" list
+        
         for word in words:
-            # then, remove (strip) unwanted punctuations from every word
-            # "dream." => "dream"
+            
             for a in string.punctuation:
-                word = word.replace(a, "")
+                if a in word:
+                    if((word.index(a) == len(word) - 1) | (word.index(a) == 0)):
+                        word = word.replace(a, "")
                  
-            # check if word is not empty
             if word:
-                # append the word to "all_words" list
                 all_words.append(word)
 
-    # compute word count from all_words
     counter = Counter(all_words)
 
-    # dump to a csv file named "wordcount.csv":
-    # word,count
-    # a,12345
-    # I,23456
-    # ...
     with open("wordcount.csv", "w", newline='') as csv_file:
-        # create a csv writer from a file object (or descriptor)
+        
         writer = csv.writer(csv_file)
-        # write table head
+        
         writer.writerow(['word', 'count'])
-        # write all (word, count) pair into the csv writer
+        
         writer.writerows(counter.most_common())
 
-    # dump to a json file named "wordcount.json"
+    
     with open("wordcount.json", "w") as json_file:
         writer = json.dump(counter.most_common(), json_file)
 
-    # BONUS: dump to a pickle file named "wordcount.pkl"
-    # hint: dump the Counter object directly
+    
     with open("wordcount.pkl", "wb") as pkl_file:
         writer = pickle.dump(counter.most_common(), pkl_file)
 
 
+# In[3]:
+
+
 if __name__ == '__main__':
     main("i_have_a_dream.txt")
+
